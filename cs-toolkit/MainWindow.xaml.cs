fde44caf499383cfbfc27e54699d3d880e5648e7
OStubbs <45260598+OStubbs@users.noreply.github.com>
2023-04-02 18:53:07 +0100

    Base
    A starting point with a ping function ready for the development of the project.

---
+++ cs-toolkit/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.NetworkInformation;
+
+namespace cs_toolkit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static PingReply PingIpOrHost(string ipOrHost)
+        {
+            // Stuck with default documentation code
+
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+
+            // Use the default Ttl value which is 128,
+            // but change the fragmentation behavior.
+            options.DontFragment = true;
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            PingReply reply = pingSender.Send(ipOrHost, timeout, buffer, options);
+
+            return reply;
+        }
+        private void Submittion_Click(object sender, RoutedEventArgs e)
+        {
+            PingReply data = PingIpOrHost(ip_address.Text);
+            ReportOutput.Text = $"Round trip time: {data.RoundtripTime}\nTime to live: {data.Options.Ttl}\nBuffer size {data.Buffer.Length}";
+        }
+    }
+}
